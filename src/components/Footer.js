586694c9d27f4e696d7f2835a8f737354a7ba72b
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-30 15:12:31 +0000

    footer.js: create footer content

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                <span>Created by </span>
+                Jamie Connell Student of Code Institute
+                <span> for educational purposes only </span>
+                <span>2024</span>
+            </p>
+            <span>
+                <a href="#"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="Visit my GitHub profile (opens in a new tab)"
+                >
+                </a>
+                <a href="#"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="Visit my linkedin profile (opens in a new tab)"
+                >
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+
+export default Footer
