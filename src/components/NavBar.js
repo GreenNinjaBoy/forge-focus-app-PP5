7e7d7b9cab59ad9bec7a0b8d3e677d417b46613c
jamie connell <Jamie.Connell_10@hotmail.com>
2024-05-30 14:29:58 +0000

    NavBar.js: create navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
 
 const NavBar = () => {
     return (
-        <div>
-
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">ForgeFocus</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">About</Nav.Link>
+                        <Nav.Link href="#link">Signup/Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
