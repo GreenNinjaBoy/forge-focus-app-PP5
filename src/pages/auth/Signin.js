55b4e96758491764a29aaf9c3c9b108c21b32e69
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-04 10:27:45 +0000

    pages/auth(signup.js create Signin function)

--- src/pages/auth/Signin.js
+++ src/pages/auth/Signin.js
@@ -3,8 +3,62 @@ import { Alert } from "react-bootstrap";
 import Button from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import {useSetCurrentUser} from '../../context/CurrentUserContext';
+import {useCurrentUser, useSetCurrentUser} from '../../context/CurrentUserContext';
 import axios from "axios";
 import {setTokenTimestamp} from '../../utils/Utils';
 import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from "../../context/GlobalMessageContext";
 
+function Signin() {
+
+    const setShowGlobalSuccess = useSetShowGlobalSuccess();
+    const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
+
+    const setCurrentUser = useCurrentUser();
+
+    const [signInData, setSignInData] = useSate({
+        username: '',
+        password: ''
+    });
+
+    const {username, password } = signInData;
+
+    const history = useHistory();
+
+    const [errors,setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event => {
+        event.prevent.default();
+        try{
+            const {data} = await axios.post('/dj-rest-auth/login/', signInData);
+            setCurrentUser(data.user);
+            setTokenTimestamp(data);
+            setGlobalSuccessMessage("You are onw signed in.");
+            setShowGlobalSuccess(true);
+            history.push('/');
+        } catch(err){
+            console.log(err)
+            setErrors(err.response?.data);
+        }
+    };
+
+  return (
+    <div>
+        <div>
+            <div>
+            <img/>
+            <h1>Log Into Your user Account</h1>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Signin
