9d128851a86789fbec82363a224729a451a607d2
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-05 07:57:16 +0000

    Miscellaneous.js: add imports to file

---
+++ src/pages/organise/Miscellaneous.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useCheckedUser, useCurrentUser } from '../../context/CurrentUserContext';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+
+
+const Miscellaneous = () => {
+  return (
+    <div>Miscellaneous</div>
+  )
+}
+
+export default Miscellaneous
