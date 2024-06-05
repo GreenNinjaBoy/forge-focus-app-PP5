63346e3f12c8f1c7a108e659d5a297a3465235d6
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-05 07:30:20 +0000

    api( create axios defaults file)

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,14 @@
+/* This code snippit was taken from the 
+"Moments" walkthrough project. */
+
+import axios from "axios";
+
+axios.defaults.basURL = 'https://forge-focus-bad8bb2ca164.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
+// This line of code intercepts the request
+export const axiosReq = axios.create();
+
+// This line of code intercepts the response
+export const axiosRes = axios.create();
