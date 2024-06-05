ea1ab8f1c42d9e48f92c3c762a681535ec55593a
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-05 07:37:20 +0000

    orangiseDetails.js: add required imports

---
+++ src/pages/organise/OrganiseDetails.js
@@ -0,0 +1,13 @@
+import React, {useContext, useEffect, useState }from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Accordion, Card, Spinner } from 'react-bootstrap';
+import { useAccordionToggle, AccordionContext } from 'react-bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
+
+const OrganiseDetails = () => {
+  return (
+    <div>OrganiseDetails</div>
+  )
+}
+
+export default OrganiseDetails
