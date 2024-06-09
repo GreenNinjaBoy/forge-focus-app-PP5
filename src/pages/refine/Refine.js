56ba65916e1516667e233d9849605e6a9b3e2210
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-09 20:32:38 +0000

    refine(refine.js: add content to file)

---
+++ src/pages/refine/Refine.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useCheckedUser, useCurrentUser } from '../../context/CurrentUserContext':
+import { useParams } from 'react-router-dom/cjs/react-router-dom'
+
+const Refine = () => {
+
+    const currentUser = useCurrentUser();
+    const checkedUser = useCheckedUser();
+    const { id } = useParams();
+
+  return (
+    <div>
+        {checkedUser ? (
+            <>
+                <div>
+                    <h1>Time to Refine {currentUser.username}</h1>
+                </div>
+                <div>This is for mobile refinement</div>
+                <div>This is for desktop refinement</div>
+            </>
+        ) : (
+            <div>
+                Just gettign your Refinement Information ....
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Refine
