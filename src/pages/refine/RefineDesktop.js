f1553d559bfbd775245f2551fc2610e40011dd28
jamie connell <Jamie.Connell_10@hotmail.com>
2024-06-09 20:37:22 +0000

    refineDesktop.js: add basic function

---
+++ src/pages/refine/RefineDesktop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+function RefineDesktop() {
+  return (
+    <div>RefineDesktop</div>
+  )
+}
+
+export default RefineDesktop
