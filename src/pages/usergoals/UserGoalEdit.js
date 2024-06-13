872097850336f24b35eaed25417de717b4df5d89
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-13 12:25:24 +0000

    UserGoalEdit.js: add convertedDate function

--- src/pages/usergoals/UserGoalEdit.js
+++ src/pages/usergoals/UserGoalEdit.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Alert, Button, Form } from 'bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useGlobalSuccessMessage, useSetShowGlobalSuccess } from '../../context/GlobalMessageContext';
 
-function UserGoalEdit() {
-  return (
-    <div>UserGoalEdit</div>
-  )
+const UserGoalEdit = (props) => {
+  const{
+    id,
+    refine,
+    title,
+    description,
+    value,
+    criteria,
+    achieve_by,
+    usergoals,
+    setUserGoals,
+    setUserGoalState
+  } = props;
+
+  const setShowGlobalSuccess = useSetShowGlobalSuccess();
+  const setGlobalSuccessMessage = useGlobalSuccessMessage();
+
+  const convertedDate = () => {
+    if (achieve_by !== null){
+      return new Date(achieve_by).toISOString().split('T')[0];
+    } else {
+      return '';
+    }
+  };
+
+  
 }
 
 export default UserGoalEdit
