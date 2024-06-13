f9e69a4291aa9cde1e78ee73394fb7f63676a4da
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-13 13:36:01 +0000

    IndividualUserGoals: create function

--- src/pages/usergoals/IndividualUserGoals.js
+++ src/pages/usergoals/IndividualUserGoals.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useContext, useState} from 'react';
+import { useAccordionToggle, AccordionContext } from 'react-bootstrap';
+import Accordion from 'react-bootstrap';
+import Card from 'react-bootstrap';
+import UserGoalsView from './UserGoalsView';
+import UserGoalEdit from './UserGoalEdit';
+import AssignmentList from '../assignments/AssignmentList';
+import UserGoalDelete from './UserGoalDelete';
 
-function IndividualUserGoals() {
+const IndividualUserGoals = (props) => {
+
+  const {
+    refine_id,
+    goal,
+    usergoals,
+    setUserGoals,
+  } = props;
+
+  const [userGoalState, setUserGoalState] = useState("view");
+
+  function UserGoalContext() {
+    if(userGoalState==='view') {
+      return <UserGoalsView{...usergoals} goals={goals} setUserGoals={setUserGoals} setUserGoalState={setUserGoalState} />
+    } else if (userGoalState==='edit') {
+      return <UserGoalEdit{...usergoals} goals={goals} setUserGoals={setUserGoals} setUserGoalState={setUserGoalState} />
+    } else if (userGoalState==='delete') {
+      return <UserGoalDelete{...usergoals} goals={goals} setUserGoals={setUserGoals} setUserGoalState={setUserGoalState} />
+    }
+  };
+
+  
   return (
     <div>IndividualUserGoals</div>
   )
