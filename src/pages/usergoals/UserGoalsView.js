32e8005b08d36d8861f1d3b9ef5beacfc1970b9d
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-17 21:17:44 +0000

    UserGoalsView.js add return statement for file

--- src/pages/usergoals/UserGoalsView.js
+++ src/pages/usergoals/UserGoalsView.js
@@ -16,9 +16,37 @@ const UserGoalsView = () => {
     setUserGoalState('edit');
   }
 
-  
+  const handleDelete = () => {
+    setUserGoalState('delete');
+  }
+
   return (
-    <div>UserGoalsView</div>
+    <>
+      <div>
+        <h3>{goaltitle}</h3>
+        <p><span>Description:</span>{description}</p>
+        <p><span>Value:</span>{value}</p>
+        <p><span>Success Criteria:</span>{criteria}</p>
+        <div>
+          {achieve_by ? (
+            <p><span>Achieve by:</span>{achieve_by}</p>
+          ) : (
+            <p>No deadline</p>
+          )}
+        </div>
+        {deadline_near && (
+          <p>DEADLINE NEAR only {days_remaining} days remaining</p>
+        )}
+        <div>
+          <button onClick={handleEdit} aria-label="Click to edit focus">
+            <i className="fa-solid fa-pen-to-square"></i>
+          </button>
+          <button onClick={handleDelete}aria-label="Click to delete focus">
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
