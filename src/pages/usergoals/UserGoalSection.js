3e9417686aeaad5e33bd30b574e80e18fdfb5d16
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-17 08:04:27 +0000

    UserGoalSection.js add handleCreate function

--- src/pages/usergoals/UserGoalSection.js
+++ src/pages/usergoals/UserGoalSection.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { axiosReq } from '../../api/axiosDefaults'
+import { Button, Spinner  } from 'react-bootstrap'
+import UserGoalsView from './UserGoalsView'
+import UserGoalCreate from './UserGoalCreate'
+import UserGoalEdit from './UserGoalEdit'
+import UserGoalDelete from './UserGoalDelete'
+import AssignmentList from '../assignments/AssignmentList'
 
-function UserGoalSection() {
+const UserGoalSection = (props) => {
+  const {
+    keyParameters,
+    setKeyParameters,
+  } = props;
+
+  const {
+    refine_id,
+    userGoal_id,
+  } = keyParameters
+
+  const [usergoals, setUserGoals] = useState({ results: []});
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [currentUserGoal, setCurrentUserGoal] = useState();
+  const [userGoalState, setUserGoalState] = useState("view");
+
+  const handleCreate = () => {
+    setUserGoalState("create");
+    setKeyParameters({
+      ...keyParameters,
+      refine_id: '',
+    });
+  }
+
+  
+
+
+
   return (
     <div>UserGoalSection</div>
   )
