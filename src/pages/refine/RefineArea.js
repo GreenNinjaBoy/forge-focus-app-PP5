2074d8415606a6b4cacde9eefb5def594c846d2e
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-17 21:42:23 +0000

    RefineArea.js: add use effect to retch refine information

--- src/pages/refine/RefineArea.js
+++ src/pages/refine/RefineArea.js
@@ -1,6 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
+import RefineView from './RefineView';
+import { Spinner } from 'react-bootstrap';
+import RefineEdit from './RefineEdit';
+import RefineDelete from './RefineDelete';
 
-function RefineArea() {
+
+
+
+const RefineArea = ( {id} ) => {
+
+  const [refineData, setRefineData] = useState({
+    name: "",
+    why: "",
+    image: "",
+
+  });
+
+  const [refineState, setRefineState] = useState("view");
+
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchRefine = async () => {
+      try {
+        const { data } = await axiosReq.get(`/Refine/${id}`)
+        const {name, why, image} = data;
+        setRefineData({name, why, image});
+        setHasLoaded(true);
+      } catch(err){
+        if (err.response?.status === 401) {
+          history.push(`/signin`);
+        } else if (err.response?.status === 403) {
+          history.push(`/organise`);
+        } else if (err.response?.status === 404) {
+          history.push('/organise');
+        }
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    fetchRefine();
+  }, [history, id])
+
+  
   return (
     <div>RefineArea</div>
   )
