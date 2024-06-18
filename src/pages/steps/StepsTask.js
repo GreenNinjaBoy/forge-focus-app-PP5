e5d881a61fc6e53e0783d173dfc85895a1eb78d9
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-18 21:03:17 +0000

    StepsTask.js: add imports and variables

--- src/pages/steps/StepsTask.js
+++ src/pages/steps/StepsTask.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {axiosReq} from '../../api/axiosDefaults';
+import {link} from 'react-router-dom/cjs/react-router-dom';
+import {useSetGlobalSuccessMessage, useSetGlobalSuccessMessage, useSetShowGlobalSuccess} from '../../context/GlobalMessageContext';
 
-function StepsTask() {
+const StepsTask = (props) => {
+  const {
+    id,
+    name,
+    image,
+    context,
+    today,
+    achieved,
+    refine,
+    achieve_by_info,
+    usergoal_achieve_by_info,
+    type,
+    activeTasks,
+    setActiveTasks,
+  } = props;
+
+  const setShowGlobalSuccess = useSetShowGlobalSuccess();
+  const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
   return (
     <div>StepsTask</div>
   )
