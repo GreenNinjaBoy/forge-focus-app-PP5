68a112416776dd00976638f19a60cd2e76d4b803
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-18 12:08:48 +0000

    RefineView.js: add vairiables and return statement

--- src/pages/refine/RefineView.js
+++ src/pages/refine/RefineView.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
-function RefineView() {
+const RefineView = (props) => {
+  const {
+    name,
+    why,
+    image,
+    setRefineState,
+  } = props;
+
+  const handleEdit = () => {
+    setRefineState('edit');
+  }
+
+  const handleDelete = () => {
+    setRefineState('delete');
+  }
+
   return (
-    <div>RefineView</div>
+    <div>
+      <img src={image} alt='refinement'/>
+      <div>
+        <h2>{name}</h2>
+        <p>{why}</p>
+        <div>
+          <button aria-label="Click to edit refinement" onClick={handleEdit}>
+            <i className="fa-solid fa-pen-to-square"></i>
+          </button>
+          <button aria-label="Click to delete refinement" onClick={handleDelete}>
+            <i className="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
