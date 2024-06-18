54bb73384aab1608ad6e47d1c93e7f58c7741ade
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-18 21:13:33 +0000

    StepsTask.js: add HandleTodayToggle function

--- src/pages/steps/StepsTask.js
+++ src/pages/steps/StepsTask.js
@@ -15,12 +15,55 @@ const StepsTask = (props) => {
     achieve_by_info,
     usergoal_achieve_by_info,
     type,
-    activeTasks,
-    setActiveTasks,
+    activeAssignments,
+    setActiveAssignments,
   } = props;
 
   const setShowGlobalSuccess = useSetShowGlobalSuccess();
   const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
+
+  const handleTodayToggle = async (event) => {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+      try {
+        const {data} = await axiosReq.patch(`/assignments/${id}`, {today: true});
+        setGlobalSuccessMessage("Assignment moved into today");
+        setShowGlobalSuccess(true);
+        const activeList = activeAssignments.results;
+        const assignmentIndex = activeList.findIndex( assignment => assignment.id === id);
+        activeList[assignmentIndex] = data;
+        setActiveAssignments(
+          {
+            results: [
+              ...activeList
+            ]
+          }
+        );
+      } catch(err) {
+        console.log(err)
+      }
+    } else {
+      try {
+        const {data} = await axiosReq.patch(`/assignments/${id}`, {today: false});
+        setGlobalSuccessMessage("Assignment removed from today");
+        setShowGlobalSuccess(true);
+        const activeList = activeAssignments.results;
+        const assignmentIndex = activeList.findIndex( assignment => assignment.id === id);
+        activeList[assignmentIndex] = data;
+        setActiveAssignments(
+          {
+            results: [
+              ...activeList
+            ]
+          }
+        );
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  }
+  
+    }
   return (
     <div>StepsTask</div>
   )
