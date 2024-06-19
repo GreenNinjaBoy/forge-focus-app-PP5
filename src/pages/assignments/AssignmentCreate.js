6869944b84cd3b79e093357ab523a06a5d14209f
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-19 12:14:35 +0000

    AssignmentCreate.js: add global variables

--- src/pages/assignments/AssignmentCreate.js
+++ src/pages/assignments/AssignmentCreate.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Alert, Form } from 'react-bootstrap';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useSetGlobalSuccessMessage, useShowGlobalSuccess } from '../../context/GlobalMessageContext';
 
-function AssignmentCreate() {
+const AssignmentCreate = ( props) => {
+  const {
+    refine_id,
+    usergoal_id,
+    assignments,
+    setAssingments,
+    type
+  } = props;
+
+  const setShowGlobalSuccess = useSetGlobalSuccess();
+  const setGlobalSuccessMessage = useSetGlobalSuccessMessage();
+
+  const assignmentList = assignments.results;
+
+  const [assignmentData, setAssignmentData] = useState ({
+    name:'',
+    refine: refine_id,
+    usergoal: usergoal_id,
+    achieve_by: '',
+  });
+
+  const {
+    name,
+    refine,
+    usergoal,
+    achieve_by,
+  } = assignmentData;
+
+  const [errors, setErrors] = useState({});
+
+  
   return (
     <div>AssignmentCreate</div>
   )
