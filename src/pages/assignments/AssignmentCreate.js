cbbf5d3fc6ff6e817d92d91122ae410f01be98ca
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-19 12:32:59 +0000

    Assignmentcreate.js: add handleSubmit function

--- src/pages/assignments/AssignmentCreate.js
+++ src/pages/assignments/AssignmentCreate.js
@@ -33,6 +33,45 @@ const AssignmentCreate = ( props) => {
 
   const [errors, setErrors] = useState({});
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('name', name)
+    if (refine) {
+      formData.append('refine', refine)
+    };
+    if (usergoal) {
+      formData.append('usergoal', usergoal)
+    }
+    if (achieve_by) {
+      const parts = achieve_by.split('-');
+      const date = new Date(psrts[0], parts[1] -1, parts[2], 12);
+      const djangoDate = date.toISOString();
+      formData.append('achieve_by', djangoDate);
+    }
+    try {
+      const {data} = await axiosReq.post(`/assignments/`, formData);
+      setGlobalSuccessMessage("New assignment created");
+      setShowGlobalSuccess(true);
+      setAssignments(
+        { results: [
+          ...assignmentList,
+          data
+        ]}
+      );
+      setAssignmentData({
+        name: '',
+        refine: refine_id,
+        usergoal: usergoal_id,
+        achieve_by: '',
+      });
+    } catch(err){
+      console.log(err);
+      if (err.response?.status !==401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
   
   return (
     <div>AssignmentCreate</div>
