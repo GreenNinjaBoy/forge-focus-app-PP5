ade7cba32cb8b46db6d121c8b10c4cae3283c1a9
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-19 20:30:10 +0000

    AssignmentDelete.js: add import statements

--- src/pages/assignments/AssignmentDelete.js
+++ src/pages/assignments/AssignmentDelete.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { axiosRes } from '../../api/axiosDefaults';
+import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from '../../context/GlobalMessageContext';
 
-function AssignmentDelete() {
+const AssignmentDelete = (props) => {
+  const {
+    const {
+      id,
+      name,
+      assignments,
+      setAssignments,
+      setAssignmentState
+    } = props;
+  
+    const setShowGlobalSuccess = useSetShowGlobalSuccess();
+    const setGlobalSuccessMessage = useSetGlobalSuccessMessage();  
+  
+    const assignmentList = assignments.results;
+  
+    const handleCancel = () => {
+      setAssignmentState('view');
+    };
+  }
+
+
   return (
     <div>AssignmentDelete</div>
   )
