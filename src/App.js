65aee62db83a82a702f87382e3dec6e0b3f1eb24
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-20 13:34:20 +0000

    revert back to previous build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch,} from 'react-router-dom/cjs/react-router-dom';
+import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom/cjs/react-router-dom';
 import './api/axiosDefaults';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom';
 import NavBar from './components/NavBar';
@@ -17,7 +17,6 @@ import NotFound from './NotFound';
 import { useEffect, useState } from 'react';
 import { useCurrentUser } from './context/CurrentUserContext';
 import styles from './App.css';
-import RefineCreate from './pages/refine/RefineCreate';
 
 
 function App() {
@@ -25,7 +24,6 @@ function App() {
   const currentUser = useCurrentUser();
   const [authenticatedUser, setAuthenticatedUser] = useState(false);
   const [tokensChecked, setTokensChecked] = useState(false);
-
   useEffect(() => {
     const checkTokens = () => {
       const refreshTokenTimeStamp = localStorage.getItem('refreshTokenTimestamp');
@@ -38,15 +36,14 @@ function App() {
     };
     checkTokens();
   }, [currentUser])
-
   return (
     <div className={styles.App}>
       <div>
-        <NavBar />
-        <SuccessMessage />
-        <div>
           {tokensChecked ? (
-            <Switch>
+          <Router className={styles.App}>
+          <NavBar />
+          <SuccessMessage />
+          <Switch>
             <Route exact path="/" render={() => <Home />} />
             <Route exact path="/signup" render={() => <Signup />} />
             <Route exact path="/signin" render={() => <SignIn />} />
@@ -57,7 +54,7 @@ function App() {
               authenticatedUser ? ( <Miscellaneous /> ) : ( <Redirect to={{pathname: "/signin"}} />)
             )} />
             <Route exact path="/refine/create" render={() => (
-              authenticatedUser ? ( <RefineCreate /> ) : ( <Redirect to={{pathname: "/signin"}} />)
+              authenticatedUser ? ( <Refinement /> ) : ( <Redirect to={{pathname: "/signin"}} />)
             )} />
             <Route exact path="/refine/:id" render={() => (
               authenticatedUser? ( <Refine /> ) : ( <Redirect to={{pathname: "/signin"}} />)
@@ -67,12 +64,12 @@ function App() {
             )} />
             <Route render={() => <NotFound />} />
           </Switch>
-      ) : (
-        <div>
+        </Router>
+              ) : (
+                <div>
                   Just checking authentication status ....
                 </div>
               )}
-          </div>
           <Footer />
         </div>
       </div>
