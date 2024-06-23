52fc953b46f3d8b6b6ca36c40b83e2024fe34b26
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-23 08:19:50 +0000

    TakeSteps.js: update files from backend build

--- src/pages/steps/TakeSteps.js
+++ src/pages/steps/TakeSteps.js
@@ -1,180 +1,186 @@
-import React, {useEffect, useState} from 'react';
-import StepsTask from './StepsTask';
-import { Form, Spinner } from 'react-bootstrap';
-import { axiosReq } from '../../api/axiosDefaults';
+import React, { useEffect, useState } from 'react';
+import { useCheckedUser, useCurrentUser } from '../../context/CurrentUserContext.js';
+import { Button, Modal } from 'react-bootstrap';
+import styles from '../../styles/TakeSteps.module.css';
+import pageStyles from '/../../styles/Page.module.css';
+import btnStyles from '../../styles/Button.module.css';
+import TakeStepsMobile from './TakeStepsMobile.js'
+import TakeStepsDesktop from './TakeStepsDesktop.js'
+import { axiosReq, axiosRes } from '../../api/axiosDefaults.js';
+import AssignmentsActionCreate from '../assignments/AssignmentsActionCreate.js';
+import { useSetGlobalSuccessMessage, useSetShowGlobalSuccess } from '../../context/GlobalMessageContext.js';
 
-const TakeStepsDesktop = (props) => {
-  const {
-    hasLoaded,
-    activeAssignments,
-    setActiveAssignments,
-    activeList,
-    todayList,
-    achievedList,
-    setHasLoaded
-  } = props;
+const TakeSteps = () => {
+  const currentUser = useCurrentUser();
+  const checkedUser = useCheckedUser();
 
-  const [filter, setFilter] = useState("");
-  const [query, setQuery] = useState("");
-  const [searchList, setSearchList] = useState({ results: []});
+  const setShowGlobalSuccess = useSetShowGlobalSuccess();
+  const setGlobalSuccessMessage = useSetGlobalSuccessMessage();  
 
-  const handleFilter = (event) => {
-    setFilter(event.target.value);
-  }
-
-  useEffect(() => {
-    if (query !== ""){
-      const updateSearchList = async () => {
-        try {
-          setHasLoaded(false);
-          const {data} = await axiosReq.get(`/assingment/?search=${query}`);
-          setSearchList(data);
-          setHasLoaded(true);
-        }  catch(err) {
-          //console.log(err)
-        }
-      };
-      // Below sets fetchPosts to fire after a 1 second pause
-      const timer = setTimeout(() => {
-        updateSearchList();
-      }, 1000)
-      // Below cleans up and clears the timeout function
-      return () => {
-        clearTimeout(timer)
-      }
-    } else {
-      setSearchList({ results: []});
-    }
-  }, [query, setHasLoaded])
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [activeAssignments, setActiveAssignments] = useState({ results: []});
+  const [activeList, setActiveList] = useState([]);
+  const [todayList, setTodayList] = useState([]);
+  const [achievedList, setAchievedList] = useState([]);
+  const [showModal, setShowModel] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
-    const changeActiveAssignmentsOrder = async () => {
+    const fetchAssignments = async () => {
       try {
-        const {data} = await axiosReq.get(`/assignment/${filter}`);
+        const {data} = await axiosReq.get('/assignment/');
         setActiveAssignments(data);
         setHasLoaded(true);
-      }  catch(err) {
+      } catch(err) {
         //console.log(err)
       }
     };
     setHasLoaded(false);
-    changeActiveAssignmentsOrder();
-  }, [filter, setActiveAssignments, setHasLoaded])
+    if (checkedUser) {
+      fetchAssignments();
+    }
+  }, [checkedUser]);
+
+  useEffect(() => {
+    if (activeAssignments.results.length>0) {
+      setActiveList(activeAssignments.results);
+      setTodayList(activeAssignments.results.filter(assignment => assignment.today === true && assignment.achieved === false));
+      setAchievedList(activeAssignments.results.filter(assignment => assignment.achieved === true));
+    }
+  }, [activeAssignments]);
+
+  const handleOpenForm = () => {
+    setShowForm(true);
+  };
+
+  const handleFormClose = () => {
+    setShowForm(false);
+  }
+
+  const handleResetRequest = () => {
+    setShowModel(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModel(false);
+  };
+
+  const handleReset = async () => {
+    if (achievedList.length > 0){
+      for (const assignment of achievedList) {
+        try {
+          const { id } = assignment;
+          await axiosRes.delete(`/assignment/${id}`)
+          setGlobalSuccessMessage("You have reset your steps board. All assignments that were complete have been deleted and everything else returned to the backlog.");
+          setShowGlobalSuccess(true);
+          const activeList = activeAssignments.results;
+          const assignmentIndex = activeList.findIndex(assignment => assignment.id === id);
+          activeList.splice(assignmentIndex, 1);
+          setActiveAssignments(
+            {
+              results: [
+                ...activeList
+              ]
+            }
+          );
+        } catch(err){
+          //console.log(err);
+        }
+      };
+    };
+    if (todayList.length > 0){
+      for (const assignment of todayList) {
+        const { id } = assignment;
+        const {data} = await axiosReq.patch(`/assignment/${id}`, { today: false });
+        setGlobalSuccessMessage("You have reset your steps board. All assignments that were complete have been deleted and everything else returned to the backlog.");
+        setShowGlobalSuccess(true);
+        const activeList = activeAssignments.results;
+        const assignmentIndex = activeList.findIndex(assignment => assignment.id === id);
+        activeList[assignmentIndex] = data;
+        setActiveAssignments(
+          {
+            results: [
+              ...activeList
+            ]
+          }
+        );
+      };
+    };
+    setShowModel(false);
+  };
 
   return (
-    <div>
-
-      <div>
-        <div>
-          <h2>Still to Complete</h2>
-          <div>
-            <Form onSubmit={(event) => event.preventDefault()}>
-              <Form.Control
-                type="text"
-                placeholder="Search assignments"
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}/>
-            </Form>
+    <div className={pageStyles.PageContainer}>
+      {checkedUser ? (
+        <>
+          <div className={pageStyles.Title}>
+            <h1>Ready to Add in Steps?<span className={styles.ExtraInfo}>{currentUser.username}</span></h1>
           </div>
-          <div>
-            <label htmlFor="filter">Order by:</label>
-            <select id="filter" name="filter" onChange={handleFilter}>
-              <option name="filter" value='?ordering=achieve_by'>Assignment deadline</option>
-              <option name="filter" value='?ordering=-refine'>Refine Area</option>
-              <option name="filter" value='?ordering=usergoal__achieve_by'>Set Goals</option>
-              <option name="filter" value='?ordering=-created_at'>Newest Assignment</option>
-            </select>
+          <div className={styles.ButtonContainer}>
+            <Button className={btnStyles.Button} onClick={handleOpenForm}>
+              Add <span>additional </span>assignments
+            </Button>
+            <Button className={btnStyles.Button} onClick={handleResetRequest}>
+             Reset 
+            </Button>
           </div>
-        </div>
-        <div>
-          {hasLoaded ? (
-            searchList.results.length>0 ? (
-              searchList.results.map( assignment => (
-                <StepsTask 
-                  key={assignment.id}
-                  {...assignment}
-                  activeTasks={activeAssignments}
-                  setActiveTasks={setActiveAssignments} 
-                  type="active"/>
-              ))
-            ) : (
-              activeList?.length>0 ? (
-                activeList.map( assignment => (
-                  <StepsTask 
-                    key={assignment.id}
-                    {...assignment}
-                    activeAssignments={activeAssignments}
-                    setActiveAssignments={setActiveAssignments} 
-                    type="active"/>
-                ))
-              ) : (
-                <p>No assignemnts found.</p>
-              )
-            )
-          ) : (
-            <div>
-              <Spinner animation="border" />
-              <p>Loading assignemnts</p>
-            </div>
-          )}
-        </div>
-      </div>
 
-      <div>
-        <div>
-          <h2>Today</h2>
-        </div>
-        <div>
-          {hasLoaded ? (
-            todayList?.length>0 ? (
-              todayList.map( assignment => (
-                <StepsTask 
-                  key={assignment.id}
-                  {...assignment}
-                  activeAssignemnts={activeAssignments}
-                  setActiveAssignments={setActiveAssignments} 
-                  type="today"/>
-              ))
-            ) : (
-              <p>No assignments set for today!</p>
-            )
-          ) : (
-            <div>
-              <Spinner animation="border" />
-              <p>Assingments Loading</p>
-            </div>
-          )}
-        </div>
-      </div>
+          <Modal show={showModal} onHide={handleModalClose}>
+            <Modal.Header closeButton>
+              <Modal.Title className={styles.ModalTitle}>Reset</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Are you sure you wish to reset "Taking Steps"?</p>
+              <p>Clicking to <strong>Reset</strong> will delete any set assignments shown and move everything back into the backlog clearing today.</p>
+              <p>If there are any assignments you wish to keep and repeat unclick done before clicking to reset.</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={handleModalClose}>
+                Cancel
+              </Button>
+              <Button onClick={handleReset}>
+                Reset Taking Steps
+              </Button>
+            </Modal.Footer>
+          </Modal>
 
-      <div>
+          <Modal show={showForm} onHide={handleFormClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Create New Assignments</Modal.Title>
+            </Modal.Header>
+            <AssignmentsActionCreate activeAssignments={activeAssignments} setActiveAssignments={setActiveAssignments} setShowForm={setShowForm}/>
+          </Modal>
+
+          <div className={styles.ModalTitle}>
+            <TakeStepsMobile 
+              hasLoaded={hasLoaded}
+              activeAssignments={activeAssignments}
+              setActiveAssignments={setActiveAssignments}
+              activeList={activeList}
+              todayList={todayList}
+              achievedList={achievedList}
+              setHasLoaded={setHasLoaded}
+            />
+          </div>
+          <div className={pageStyles.DesktopOnly}>
+            <TakeStepsDesktop 
+              hasLoaded={hasLoaded}
+              activeAssignments={activeAssignments}
+              setActiveAssignments={setActiveAssignments}
+              activeList={activeList}
+              todayList={todayList}
+              achievedList={achievedList}
+              setHasLoaded={setHasLoaded}
+            />
+          </div>
+        </>
+      ) : (
         <div>
-          <h2>Completed</h2>
+          Just loading your information ....
         </div>
-        <div>
-        {hasLoaded ? (
-            achievedList?.length>0 ? (
-              achievedList.map( assignment => (
-                <StepsTask 
-                  key={assignment.id}
-                  {...assignment}
-                  activeAssignments={activeAssignments}
-                  setActiveAssignments={setActiveAssignments} 
-                  type="achieved"/>
-              ))
-            ) : (
-              <p>No Completed Assignments!</p>
-            )
-          ) : (
-            <div>
-              <Spinner animation="border" />
-              <p>Assingments Loading</p>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+      )}
+    </div>  
   )
 }
 
-export default TakeStepsDesktop
+export default TakeSteps
