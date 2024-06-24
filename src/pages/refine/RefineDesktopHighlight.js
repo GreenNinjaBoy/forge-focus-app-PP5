9368dd54dcb7727544e9c1b9ed3b29fca71e2b4a
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-24 09:59:08 +0000

    RefineDesktopHighlight.js: update file from backend

--- src/pages/refine/RefineDesktopHighlight.js
+++ src/pages/refine/RefineDesktopHighlight.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import btnStyles from '../../styles/Button.module.css';
+import cardStyles from '../../styles/Cards.module.css';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 import { axiosReq } from '../../api/axiosDefaults';
 import { Spinner } from 'react-bootstrap';
@@ -19,7 +21,7 @@ const RefineDesktopHighlight = (props) => {
   useEffect(() => {
     const fetchUserGoals = async () => {
       try {
-        const {data} = await axiosReq.get(`/usergoals/?refine_id=${id}`);
+        const {data} = await axiosReq.get(`/goals/?refine_id=${id}`);
         setUserGoals(data);
         setHasLoaded(true);
       } catch(err) {
@@ -31,15 +33,15 @@ const RefineDesktopHighlight = (props) => {
   }, [id]);
   
   return (
-    <div>
-    <div>
-      <img src={image} alt={`${name} refine`}/>
-      <div>
+    <div className={cardStyles.Card}>
+    <div className={cardStyles.Header}>
+      <img className={cardStyles.Image}  src={image} alt={`${name} refine`}/>
+      <div className={cardStyles.Title}>
         <h2>{name}</h2>
         <p>{why}</p>
       </div>
     </div>
-    <div>
+    <div className={cardStyles.GoalContainer}>
       {hasLoaded ? (
         <>
           {userGoals.results.length ? (
@@ -47,22 +49,22 @@ const RefineDesktopHighlight = (props) => {
               <UserGoalHighlight key={userGoals.id} {...userGoals} />
             ))
           ) : (
-            <div>
+            <div className={cardStyles.AddPadding}>
               <p>You don't have any goals set for this refinement.</p>
             </div>
           )}
         </>
       ) : (
         <>
-        <div>
+        <div className={cardStyles.SpinnerContainer}>
           <Spinner animation="border" />
-          <p>We are just loading your goals</p>
+          <p>Loading User Goals</p>
         </div>
         </>
       )}
     </div>
-    <div>
-      <Link to={`/refine/${id}`}>
+    <div className={cardStyles.ButtonContainer}>
+      <Link  className={`${btnStyles.Button} ${cardStyles.Button}`} to={`/refine/${id}`}>
         Go to refinement
       </Link>
     </div>
