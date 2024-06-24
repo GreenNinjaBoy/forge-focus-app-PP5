da67cf4207d8eca8a9575bfcaae7f7bec221f5b2
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-24 09:58:46 +0000

    RefineDesktop.js: update file from backend

--- src/pages/refine/RefineDesktop.js
+++ src/pages/refine/RefineDesktop.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import styles from '../../styles/RefineDesktop.module.css';
+import btnStyles from '../../styles/Button.module.css';
+import pageStyles from '../../styles/Page.module.css';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import UserGoalSection from '../usergoals/UserGoalSection';
 import RefineArea from './RefineArea';
@@ -19,19 +22,19 @@ const RefineDesktop = ({ id }) => {
   }
 
   return (
-    <div>
-      <button aria-label="Click to return to the plan" onClick={handleBack}>
+    <div className={`${pageStyles.ContentContainer} ${styles.MainContainer}`}>
+      <button className={btnStyles.BackCross} aria-label="Click to return to the plan" onClick={handleBack}>
         <i className="fa-solid fa-x"></i>
       </button>
 
       <RefineArea id={refine_id} />
 
-      <div>
+      <div className={styles.LinkedDetailsContainer}>
         <UserGoalSection keyParameters={keyParameters} setKeyParameters={setKeyParameters}/>
-        <div>
-          <div>
+        <div className={styles.DayToDayContainer}>
+          <div className={styles.DayToDayInfo}>
             <h3>Day to day steps</h3>
-            <p>(Those jobs that just need doing)</p>
+            <p>(The little things that need doing!)</p>
           </div>
           <AssignmentList refine_id={refine_id} type="daytoday"/>
         </div>
