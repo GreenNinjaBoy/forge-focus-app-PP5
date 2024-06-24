a56cf4056c545bd54aa6d1dbe21dd4f862f45624
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-24 09:55:04 +0000

    Miscellaneous.js: update file from backend

--- src/pages/organise/Miscellaneous.js
+++ src/pages/organise/Miscellaneous.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import styles from '../../styles/Miscellaneous.module.css';
+import pageStyles from '../../styles/Page.module.css';
+import btnStyles from '../../styles/Button.module.css';
 import { useCheckedUser, useCurrentUser } from '../../context/CurrentUserContext';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 import AssignmentList from '../assignments/AssignmentList'
@@ -15,21 +18,21 @@ const Miscellaneous = () => {
   }
 
   return (
-    <div>
+    <div className={pageStyles.PageContainer}>
       {checkedUser ? (
         <>
-          <div>
+          <div className={pageStyles.Title}>
             <h1>{currentUser.username}'s Plan</h1>
           </div>      
-          <div>
-            <button aria-label="Click to return to the plan" onClick={handleBack}>
+          <div className={`${pageStyles.ContentContainer} ${styles.MainContainer}`}>
+            <button className={btnStyles.BackCross} aria-label="Click to return to organiser" onClick={handleBack}>
               <i className="fa-solid fa-x"></i>
             </button>
-            <div>
-              <img src='' alt='miscellaneous'/>
+            <div className={styles.Title}>
+              <img className={styles.Image} src='' alt='miscellaneous'/>
               <h2>Miscellaneous</h2>
             </div>
-            <div>
+            <div className={styles.TaskList}>
               <AssignmentList type="miscellaneous"/>
             </div>
           </div>
