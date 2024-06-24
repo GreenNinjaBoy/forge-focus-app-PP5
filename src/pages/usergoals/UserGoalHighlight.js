aca3e72650dfbf4ad562372ab0e816e7819f7b69
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-06-24 10:07:02 +0000

    UserGoalHighlight.js: update file from backend

--- src/pages/usergoals/UserGoalHighlight.js
+++ src/pages/usergoals/UserGoalHighlight.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../styles/UserGoal.module.css';
+import cardStyles from '../../styles/Cards.module.css';
 
 const UserGoalHighlight = (props) => {
 
@@ -12,20 +14,20 @@ const UserGoalHighlight = (props) => {
 
 
   return (
-    <div>
-      <div>
+    <div className={cardStyles.Goal}>
+      <div className={cardStyles.GoalTitle}>
         <h3>{title}</h3>
         <span>{achieve_by}</span>
       </div>
       {description && <p>{description}</p>}
       {achieve_by_near && (
         time_remaining===0 || time_remaining===1 ? (
-          <p>Closing in on Achieve By Date</p>
+          <p className={styles.HighlightDeadlineWarning}>Closing in on Achieve By Date</p>
         ) : (
           time_remaining<0 ? (
-            <p>You hav passed your set Achieve by date!!!!!</p>
+            <p className={styles.HighlightDeadlineWarning}>You hav passed your set Achieve by date!!!!!</p>
           ) : (
-            <p>Closing in on Achievement date only {time_remaining} days remaining!</p>
+            <p className={styles.HighlightDeadlineWarning} >Closing in on Achievement date only {time_remaining} days remaining!</p>
           )
         )
       )}
